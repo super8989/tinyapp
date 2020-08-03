4f73d742870815a71bc8c58fa74dde5a8185d505
Sam Yang <samyang8989@gmail.com>
2020-08-03 20:57:57 +0000

    added a route to display a single URL and its shortened form

--- express_server.js
+++ express_server.js
@@ -26,18 +26,18 @@ app.get('/urls', (req, res) => {
 	// res.render('urls_index', urlDatabase);
 });
 
-app.get('/hello', (req, res) => {
-	res.send('<html><body>Hello <b>World HTML</b></body></html>\n');
+app.get('/urls/:shortURL', (req, res) => {
+	let templateVars = {
+		shortURL: req.params.shortURL,
+		longURL: urlDatabase[req.params.shortURL],
+	};
+	res.render('urls_show', templateVars);
 });
+
+// app.get('/hello', (req, res) => {
+// 	res.send('<html><body>Hello <b>World HTML</b></body></html>\n');
+// });
 
 app.listen(PORT, () => {
 	console.log(`Example app listening on port ${PORT}!`);
 });
-
-
-{
-	urls: {
-		b2xVn2: 'http://www.lighthouselabs.ca',
-	'9sm5xK': 'http://www.google.com',
-	}
-}
