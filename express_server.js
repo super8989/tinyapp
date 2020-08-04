701767d91f2d6579ea2bbf895d856901e4a75474
Sam Yang <samyang8989@gmail.com>
2020-08-04 01:54:24 +0000

    added a route for redirect to longURL

--- express_server.js
+++ express_server.js
@@ -46,6 +46,13 @@ app.get('/urls/:shortURL', (req, res) => {
 	res.render('urls_show', templateVars);
 });
 
+app.get('/u/:shortURL', (req, res) => {
+	// req.params: {shortURL: <typedURL> }
+	const longURL = urlDatabase[req.params.shortURL];
+
+	res.redirect(longURL);
+});
+
 app.post('/urls', (req, res) => {
 	console.log(req.body); // {longURL: 'http://www.apple.com'}
 	const shortURL = generateRandomString();
