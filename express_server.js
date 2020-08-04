ec1741df03169a810f9985cfca4b71fa07342e1e
Sam Yang <samyang8989@gmail.com>
2020-08-04 00:06:29 +0000

    added a post route to receive form submission

--- express_server.js
+++ express_server.js
@@ -41,6 +41,11 @@ app.get('/urls/:shortURL', (req, res) => {
 	res.render('urls_show', templateVars);
 });
 
+app.post('/urls', (req, res) => {
+	console.log(req.body);
+	res.send('OK');
+});
+
 app.listen(PORT, () => {
 	console.log(`Example app listening on port ${PORT}!`);
 });
