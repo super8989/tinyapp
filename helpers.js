18fe248e2961b67ba47afb3985ffd447b04a8427
Sam Yang <samyang8989@gmail.com>
2020-08-06 23:41:24 +0000

    eslinted all files

--- helpers.js
+++ helpers.js
@@ -1,28 +1,28 @@
 // Check if submitted email is in the user database
 const checkEmail = (userDB, email) => {
-	for (user in userDB) {
-		if (userDB[user].email === email) {
-			return userDB[user];
-		}
-	}
-	return false;
+  for (user in userDB) {
+    if (userDB[user].email === email) {
+      return userDB[user];
+    }
+  }
+  return false;
 };
 
 // Filters URLs created by the current user
 const urlsForUser = (urlDB, id) => {
-	const filteredObj = {};
+  const filteredObj = {};
 
-	for (item in urlDB) {
-		if (urlDB[item].userID === id) {
-			filteredObj[item] = urlDB[item];
-		}
-	}
-	return filteredObj;
+  for (item in urlDB) {
+    if (urlDB[item].userID === id) {
+      filteredObj[item] = urlDB[item];
+    }
+  }
+  return filteredObj;
 };
 
 // return a string of 6 random alphanumeric characters
 function generateRandomString() {
-	return Math.random().toString(36).substr(2, 6);
+  return Math.random().toString(36).substr(2, 6);
 }
 
 module.exports = { checkEmail, urlsForUser, generateRandomString };
--- test/urlsForUserTest.js
+++ test/urlsForUserTest.js
@@ -3,26 +3,26 @@ const { assert } = require('chai');
 const { urlsForUser } = require('../helpers');
 
 const urlDatabase = {
-	b6UTxQ: { longURL: 'https://www.tsn.ca', userID: 'aJ48lW' },
-	i3BoGr: { longURL: 'https://www.google.ca', userID: 'aJ48lW' },
-	t8PsLm: { longURL: 'https://www.apple.ca', userID: 'cP93zw' },
+  b6UTxQ: { longURL: 'https://www.tsn.ca', userID: 'aJ48lW' },
+  i3BoGr: { longURL: 'https://www.google.ca', userID: 'aJ48lW' },
+  t8PsLm: { longURL: 'https://www.apple.ca', userID: 'cP93zw' },
 };
 
-describe('urslForUser', function () {
-	it('should return a filtered object of URLs created by the current user', function () {
-		const filteredObj = urlsForUser(urlDatabase, 'aJ48lw');
-		const expectedOutput = {
-			b6UTxQ: { longURL: 'https://www.tsn.ca', userID: 'aJ48lW' },
-			i3BoGr: { longURL: 'https://www.google.ca', userID: 'aJ48lW' },
-		};
+describe('urslForUser', function() {
+  it('should return a filtered object of URLs created by the current user', function() {
+    const filteredObj = urlsForUser(urlDatabase, 'aJ48lw');
+    const expectedOutput = {
+      b6UTxQ: { longURL: 'https://www.tsn.ca', userID: 'aJ48lW' },
+      i3BoGr: { longURL: 'https://www.google.ca', userID: 'aJ48lW' },
+    };
 
-		assert.deepEqual = (filteredObj, expectedOutput);
-	});
+    assert.deepEqual = (filteredObj, expectedOutput);
+  });
 
-	it('should return an empty object if user has not created any URLs', function () {
-		const filteredObj = urlsForUser(urlDatabase, 'nY60er');
-		const expectedOutput = {};
+  it('should return an empty object if user has not created any URLs', function() {
+    const filteredObj = urlsForUser(urlDatabase, 'nY60er');
+    const expectedOutput = {};
 
-		assert.deepEqual = (filteredObj, expectedOutput);
-	});
+    assert.deepEqual = (filteredObj, expectedOutput);
+  });
 });
